e9dcba64e93c53a7102e3fa1bc091c9313def063
Stephen Lynch <stepheng.lynch@gmail.com>
2022-01-02 16:09:39 +0000

    Day 6 done

--- day6a/src/main.rs
+++ day6a/src/main.rs
@@ -1,3 +1,46 @@
+
+fn new_fish() -> u32 {
+    8
+}
+
+fn simulate_school(mut school: Vec<u32>) -> Vec<u32> {
+    let sim_len = 80;
+
+    // println!("{:?}", school);
+
+    for _ in 0..sim_len {
+
+        let mut no_new_fish = 0;
+        
+        for f in school.iter_mut() {
+            if *f == 0 {
+                no_new_fish += 1;
+                *f = 6;
+            } else {
+                *f -= 1;
+            }
+        }
+
+        for _ in 0..no_new_fish {
+            school.push(new_fish())
+        }
+
+        // println!("{:?}", school);
+    }
+
+    school
+}
+
+fn parse_input(s: &str) -> Vec<u32> {
+    s.split(',').map(|x| x.parse().unwrap()).collect()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let raw = include_str!("input.txt");
+
+    let school = parse_input(raw);
+
+    let school = simulate_school(school);
+
+    println!("answer = {:?}", school.len())
 }
--- day6b/src/main.rs
+++ day6b/src/main.rs
@@ -1,3 +1,60 @@
+
+
+type School = [u64; 9];
+
+const sim_len: u32 = 256;
+
+fn simulate_day(s: School) -> School {
+    let max_days = 8;
+    let mut new_school = [0; 9];
+
+    // Spawned fished and reset fish
+    new_school[8] = s[0];
+    new_school[6] = s[0];
+
+    for n in 0..max_days {
+        new_school[n] += s[n+1];
+    }
+    // new_school[0] = 0[1];
+    // new_school[1] = 0[2];
+    // ...
+    // new_school[7] = 0[8];
+
+    new_school
+}
+
+fn simulate_school(mut school: School) -> School {
+    for _ in 0..sim_len {
+        school = simulate_day(school);
+    }
+
+    school
+}
+
+fn parse_input(s: &str) -> School {
+    let mut school = [0; 9];
+    let fishes= s.split(',').map(|x| -> usize {x.parse().unwrap()});
+    for f in fishes {
+        school[f] += 1;
+    }
+
+    school
+}
+
+fn no_fish(s: &School) -> u64 {
+    s.iter().sum()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let raw = include_str!("input.txt");
+
+    let school = parse_input(raw);
+
+    println!("{:?}", school);
+
+    let school = simulate_school(school);
+
+    println!("{:?}", school);
+
+    println!("answer = {:?}", no_fish(&school))
 }
